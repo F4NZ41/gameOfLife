e6d0b923b62d9eeaf9190ac8b6d123eb9210c6c6
memer <mimeremim@gmail.com>
2021-03-26 22:07:19 +0100

    bruh2

--- main.js
+++ main.js
@@ -18,26 +18,37 @@ const pg = [
 ];
 
 function ngbr() {
-    var n = 0;
-    if(pg[i-1][j-1]) {n++}
-    if(pg[i-1][j]) {n++}
-    if(pg[i-1][j+1]) {n++}
-    if(pg[i][j-1]) {n++}
-    if(pg[i][j+1]) {n++}
-    if(pg[i+1][j-1]) {n++}
-    if(pg[i+1][j]) {n++}
-    if(pg[i+1][j+1]) {n++}
-    return(n)
+
 }
 
 function render() {
     for(var i = 0; i < height; i++) {
         for(var j = 0; j < width; j++) {
+
+            var n = 0;
+
+            try { if (pg[i-1][j-1]==1){n++}} catch {console.log("catch xd")}
+            try { if (pg[i-1][j]==1) {n++} } catch {console.log("catch xd")}
+            try { if(pg[i-1][j+1]==1) {n++}} catch {console.log("catch xd")}
+            try { if (pg[i][j-1]==1) {n++} } catch {console.log("catch xd")}
+            try { if (pg[i][j+1]==1) {n++} } catch {console.log("catch xd")}
+            try { if(pg[i+1][j-1]==1) {n++}} catch {console.log("catch xd")}
+            try { if (pg[i+1][j]==1) {n++} } catch {console.log("catch xd")}
+            try { if(pg[i+1][j+1]==1) {n++}} catch {console.log("catch xd")}
+            
+            console.log(n)
+
             console.log(pg[i][j]);
+            
 
             if(pg[i][j]==1) {
                 ctx.fillRect(j*20,i*20,20,20)
             }
+
+            if(n<2) {
+                pg[i][j]=0;
+            }
+            
             
         }
     }
